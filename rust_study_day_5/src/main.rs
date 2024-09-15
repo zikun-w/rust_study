1c561f7290e5634778bcd608337a024bac295a2c
zikun-w <zikun@stu.xjtu.edu.cn>
2024-09-15 13:55:02 +0800

    if else match

--- rust_study_day_5/src/main.rs
+++ rust_study_day_5/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    // if else & match
+    let grade = 60;
+    if grade < 60 {
+        println!("You failed");
+    } else {
+        println!("You passed");
+    }
+
+    let message = if grade < 60 {"You failed"} else {"You passed"};
+
+    println!("{message}");
+
+    println!("{}", match grade {
+        60..=100 => "You passed",
+        0..=59 => "You failed",
+        _ => "Illegal grade!!!",
+    });
 }
